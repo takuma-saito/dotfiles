e4025af57a435b2587f98e3f1e953566da17ad00
takuma.saito <855739+takuma-saito@users.noreply.github.com>
2023-10-12 14:45:01 +0900

    ln の不具合対応

--- dot_config/nu/run_once_before_install_nu.sh
+++ dot_config/nu/run_once_before_install_nu.sh
@@ -4,4 +4,4 @@ mkdir -p ~/.config/nu
 zoxide init nushell > ~/.config/nu/zoxide.nu
 cp "$(brew --prefix asdf)/libexec/asdf.nu" ~/.config/nu/asdf.nu
 starship init nu > ~/.config/nu/starship.nu
-ln -sfn "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock" ~/.1password/agent.sock
+ln -sfn ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock ~/.1password/agent.sock
