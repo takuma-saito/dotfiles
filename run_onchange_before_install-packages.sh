f4a81dcc6b21f8b789adeae45d16668eb5e81485
takuma.saito <855739+takuma-saito@users.noreply.github.com>
2023-10-14 16:19:05 +0900

    defaults の設定を追加

--- run_onchange_before_install-packages.sh
+++ run_onchange_before_install-packages.sh
@@ -1,5 +1,25 @@
 #!/bin/bash -euC
 
+# https://macos-defaults.com/dock/orientation.html
+# https://developer.apple.com/documentation/devicemanagement
+
+# Dock
+defaults write com.apple.dock "orientation" -string "right"
+defaults write com.apple.dock "autohide" -bool "true"
+defaults write com.apple.dock "static-only" -bool "true"
+defaults write com.apple.dock "tilesize" -int "48"
+killall Dock || true
+
+# Finder
+defaults write com.apple.finder "AppleShowAllFiles" -bool "true"
+defaults write com.apple.finder "FXPreferredViewStyle" -string "clmv"
+defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true"
+killall Finder || true
+
+# Music
+defaults write com.apple.Music "userWantsPlaybackNotifications" -bool "false"
+killall Music || true
+
 brew bundle --file=/dev/stdin <<EOF
 # Basic Setup
 cask "google-chrome"
