0a1fc7c1fc9881ee462956d404bc44c8eb1cabc9
takuma.saito <855739+takuma-saito@users.noreply.github.com>
2023-10-21 19:15:45 +0900

    fix alacritty setup

--- private_dot_config/alacritty/run_onchange_after_install-packages.sh
+++ private_dot_config/alacritty/run_onchange_after_install-packages.sh
@@ -1,3 +1,8 @@
 #!/bin/bash -euC
 mkdir -p ~/.config/alacritty/themes
-git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
+if [[ ! -d ~/.config/alacritty/themes ]]; then
+  git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
+else
+  cd ~/.config/alacritty/themes
+  git pull origin
+fi
